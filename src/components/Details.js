9d9a2522fbc5fb5d52bd8cdffe27ce2eb32017e1
Kerem Emir Ercan <kerememirercan@gmail.com>
2025-03-03 10:15:04 +0300

    Details.js Updated

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,49 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, ActivityIndicator, ScrollView } from 'react-native';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { View, Text, ActivityIndicator, ScrollView, Image } from "react-native";
+import axios from "axios";
 
 const Details = () => {
-  const [data, setData] = useState(null);
+  const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   // https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true
+
   useEffect(() => {
     // API'den veri çekme
-    axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=85e40a4127f8493f97ff82366c7d3b96')  // API URL'sini buraya girin
+    axios
+      .get(
+        "https://api.spoonacular.com/recipes/complexSearch?apiKey=85e40a4127f8493f97ff82366c7d3b96"
+      ) // API URL'sini buraya girin
       .then((response) => {
-        setData(response.data.recipes[0]);  // Veriyi state'e ata
-        setLoading(false);  // Yükleniyor durumunu sonlandır
+        setRecipes(response.data.results); // Veriyi state'e ata
+        setLoading(false); // Yükleniyor durumunu sonlandır
       })
       .catch((error) => {
-        setError(error);  // Hata durumunu yakala
-        setLoading(false);  // Yükleniyor durumunu sonlandır
+        setError(error); // Hata durumunu yakala
+        setLoading(false); // Yükleniyor durumunu sonlandır
       });
-  }, []);  // Boş bağımlılık dizisi, sadece component mount olduğunda çalışır
+  }, []);
 
   if (loading) {
     return (
-      <View>
-        <ActivityIndicator size="large" color="#0000ff" />  {/* Yükleniyor göstergesi */}
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
       </View>
     );
   }
 
   if (error) {
     return (
-      <View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Error: {error.message}</Text>
       </View>
     );
   }
 
   return (
-    <ScrollView>
+    <ScrollView style={{ flex: 1 }}>
       <Text>Data:</Text>
-      {/* Data null veya undefined değilse veriyi göster */}
-      {data ? (
-        <Text>{JSON.stringify(data, null, 2)}</Text>  
+      {recipes.length > 0 ? (
+        recipes.map((value, index) => 
+        <View key={index}>
+          <Text>{value.title}</Text>
+          <Image style={{width:50, height:150}} source={{uri: value.image}}/>
+        </View>
+      )
       ) : (
-        <Text>No data available</Text> 
+        <Text>No data available</Text>
       )}
     </ScrollView>
   );
